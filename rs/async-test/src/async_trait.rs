ce7649073d9d2075a8f15a5b48bc7ae309ac739b
zooeywm <zooeywm@gmail.com>
2026-01-30 10:42:38 +0800

    feat: manual Future

--- rs/async-test/src/async_trait.rs
+++ rs/async-test/src/async_trait.rs
@@ -1,4 +1,6 @@
-use std::{pin::Pin, sync::Arc};
+use std::{io, pin::Pin, sync::Arc, task::{Context, Poll}};
+
+use compio::fs::File;
 
 #[allow(dead_code)]
 #[derive(Debug)]
@@ -6,8 +8,62 @@ pub struct User {
 	id: String,
 }
 
-pub trait UserServiceWithStaticFuture {
+pub trait UserServiceWithCompioStaticFuture {
 	async fn get_user(&self, id: &str) -> Option<User>;
+	/// get_user 的 desugar 版本
+	fn get_user_async_manual<'a>(&'a self, id: &'a str) -> impl Future<Output = Option<User>> + use<'a, Self>;
+}
+
+struct GetUserFuture<'a> {
+	id:    &'a str,
+	state: State<'a>,
+}
+
+enum State<'a> {
+	Start,
+	Opening(Pin<Box<dyn Future<Output = io::Result<File>> + 'a>>),
+	Reading(Pin<Box<dyn Future<Output = Option<User>> + 'a>>),
+}
+
+impl<'a> Future for GetUserFuture<'a> {
+	type Output = Option<User>;
+
+	fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+		loop {
+			match &mut self.state {
+				State::Start => {
+					let fut = Box::pin(compio::fs::File::open("Cargo.toml"));
+					self.state = State::Opening(fut);
+				}
+
+				State::Opening(fut) => {
+					let file = match fut.as_mut().poll(cx) {
+						Poll::Pending => return Poll::Pending,
+						Poll::Ready(Ok(f)) => f,
+						Poll::Ready(Err(e)) => panic!("{e:?}"),
+					};
+
+					let id = self.id.to_string();
+
+					let fut = Box::pin(async move {
+						let (_read, buffer) =
+							compio::io::AsyncReadAtExt::read_to_end_at(&file, Vec::with_capacity(1024), 0).await.unwrap();
+
+						let text = String::from_utf8(buffer).ok()?;
+						println!("{text}");
+
+						Some(User { id })
+					});
+
+					self.state = State::Reading(fut);
+				}
+
+				State::Reading(fut) => {
+					return fut.as_mut().poll(cx);
+				}
+			}
+		}
+	}
 }
 
 pub trait UserServiceWithDynamicSendFuture {
@@ -30,9 +86,14 @@ pub trait UserServiceWithDynamicFuture {
 		'id: 'future;
 }
 
-pub async fn use_service_static(service: impl UserServiceWithStaticFuture) {
+pub async fn use_service_manual_compio_static(service: impl UserServiceWithCompioStaticFuture) {
+	let user = service.get_user_async_manual("user-123").await;
+	println!("use_service_manual_compio_static: {user:?}");
+}
+
+pub async fn use_service_compio_static(service: impl UserServiceWithCompioStaticFuture) {
 	let user = service.get_user("user-123").await;
-	println!("use_service_static: {user:?}");
+	println!("use_service_compio_static: {user:?}");
 }
 
 pub async fn use_service_dynamic_compio(service: Box<dyn UserServiceWithDynamicFuture>) {
@@ -53,8 +114,20 @@ pub async fn use_service_dynamic_tokio(service: Arc<dyn UserServiceWithDynamicSe
 
 pub struct DbUserService;
 
-impl UserServiceWithStaticFuture for DbUserService {
-	async fn get_user(&self, id: &str) -> Option<User> { Some(User { id: id.to_string() }) }
+impl UserServiceWithCompioStaticFuture for DbUserService {
+	async fn get_user(&self, id: &str) -> Option<User> {
+		let file = File::open("Cargo.toml").await.unwrap();
+		let (read, buffer) =
+			compio::io::AsyncReadAtExt::read_to_end_at(&file, Vec::with_capacity(1024), 0).await.unwrap();
+		assert_eq!(read, buffer.len());
+		let buffer = String::from_utf8(buffer).unwrap();
+		println!("{buffer}");
+		Some(User { id: id.to_string() })
+	}
+
+	fn get_user_async_manual<'a>(&'a self, id: &'a str) -> impl Future<Output = Option<User>> + use<'a> {
+		GetUserFuture { id, state: State::Start }
+	}
 }
 
 impl UserServiceWithDynamicFuture for DbUserService {
--- rs/async-test/src/main.rs
+++ rs/async-test/src/main.rs
@@ -1,32 +1,19 @@
 use std::sync::Arc;
 
-use compio::{fs::File, io::AsyncReadAtExt};
-
-use crate::async_trait::{DbUserService, use_service_dynamic_compio, use_service_dynamic_tokio, use_service_static};
+use crate::async_trait::{DbUserService, use_service_compio_static, use_service_dynamic_compio, use_service_dynamic_tokio, use_service_manual_compio_static};
 
 mod async_trait;
 
 fn main() {
-	let compio_rt = compio::runtime::Runtime::new().expect("cannot create runtime");
-
-	compio_rt.block_on(async move {
-		let file = File::open("Cargo.toml").await.unwrap();
-		let (read, buffer) = file.read_to_end_at(Vec::with_capacity(1024), 0).await.unwrap();
-		assert_eq!(read, buffer.len());
-		let buffer = String::from_utf8(buffer).unwrap();
-		println!("{buffer}");
-		use_service_static(DbUserService).await;
+	compio::runtime::Runtime::new().unwrap().block_on(async {
+		use_service_manual_compio_static(DbUserService).await;
+		use_service_compio_static(DbUserService).await;
 		use_service_dynamic_compio(Box::new(DbUserService)).await;
 	});
 
-	let tokio_rt = tokio::runtime::Builder::new_multi_thread()
-		.worker_threads(4)
-		.enable_all()
-		.build()
-		.expect("create tokio runtime");
-
-	tokio_rt.block_on(async {
-		use_service_static(DbUserService).await;
-		use_service_dynamic_tokio(Arc::new(DbUserService)).await;
-	});
+	tokio::runtime::Builder::new_multi_thread().worker_threads(4).enable_all().build().unwrap().block_on(
+		async {
+			use_service_dynamic_tokio(Arc::new(DbUserService)).await;
+		},
+	);
 }
